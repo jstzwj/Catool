21c7ddb1500ad792122442477054d4983dca582c
WangJun <1103870790@qq.com>
2017-08-05 18:20:56 +0800

    Update DiagnosticEngine

--- src/Catool/DiagnosticToolBox/DiagnosticEngine.h
+++ src/Catool/DiagnosticToolBox/DiagnosticEngine.h
@@ -1,13 +1,49 @@
 #pragma once
 #ifndef CATOOL_DIAGNOSTICTOOLBOX_DIAGNOSTICENGINE_H
 #define CATOOL_DIAGNOSTICTOOLBOX_DIAGNOSTICENGINE_H
+
+#include<memory>
 namespace catool
 {
 	namespace diagnostic_toolbox
 	{
-		class DiagnosticEngine
+		class DiagnosticConsumer
+		{
+		protected:
+			unsigned NumWarnings = 0;
+			unsigned NumErrors = 0;
+		public:
+			DiagnosticConsumer() = default;
+			virtual ~DiagnosticConsumer()=default;
+			unsigned getNumErrors() const { return NumErrors; }
+			unsigned getNumWarnings() const { return NumWarnings; }
+			virtual void clear() { NumWarnings = NumErrors = 0; }
+		};
+		class DiagnosticIDs
 		{
 		public:
+
+		};
+		class DiagnosticOptions
+		{
+		public:
+
+		};
+		class DiagnosticEngine
+		{
+		private:
+			bool SuppressAfterFatalError;  // Suppress diagnostics after a fatal error?
+			bool SuppressAllDiagnostics;   // Suppress all diagnostics.
+
+			bool ShowColors;               // Color printing is enabled.
+
+			unsigned ErrorLimit;           // Cap of # errors emitted, 0 -> no limit.
+			DiagnosticIDs Diags;
+			DiagnosticOptions DiagOpts;
+			DiagnosticConsumer *Client;
+			std::unique_ptr<DiagnosticConsumer> Owner;
+		public:
+
 		};
 	}
 }
